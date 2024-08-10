6a4159463e2457019435c78c0a1080c3759dfe0c
3sam3 <iamdap91@gmail.com>
2024-08-11 02:50:46 +0900

    feat: dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+export default function Dashboard() {
+  return (
+    <>
+      <div className="flex items-center">
+        <div className="flex-auto">1</div>
+        <div className="flex-auto">2</div>
+        <div className="flex-auto">3</div>
+      </div>
+    </>
+  );
 }
