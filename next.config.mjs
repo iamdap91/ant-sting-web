2566d2e9dc241e017684633d52725b595de1fee6
3sam3 <iamdap91@gmail.com>
2024-08-22 13:40:00 +0900

    chore: dev port setting

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:3001/api/:path*",
+        destination: "http://211.106.184.250:8080/api/:path*",
       },
     ];
   },
